a8f8df2cd02232e83583ae1130e750337fefa1d1
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-06 21:17:10 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,my_list):
+        result = []
+        for i in range (len(my_list)):
+            if sorted(self.word) == sorted(my_list[i]):
+                result.append (my_list[i])
+        return result
